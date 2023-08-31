09872fe3c94703121507f0332b11020e1ebec73d
padeyao4 <guojian_k@qq.com>
2023-08-31 20:49:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Graph } from '@antv/g6';
+import { onMounted, ref } from 'vue';
+
+let container = ref<HTMLElement>()
+let graph = ref<Graph>()
+
+onMounted(() => {
+  graph.value = new Graph({
+    container: container.value!
+  })
+  graph.value.on('canvas:click', () => {
+    alert('canvas click')
+  })
+})
+
+const handlerChangeSize = () => {
+  graph.value?.changeSize(container.value!.clientWidth, container.value!.clientHeight)
+}
+
+const handleRender=()=>{
+  graph.value?.render()
+}
+
+</script>
+
+<template>
+  <div style="display: flex;">
+    <button @click="handlerChangeSize">change-size</button>
+    <button @click="handleRender">render</button>
+  </div>
+  <fieldset style="width: 400px;height: 400px;">
+    <div ref="container" style="width: 100%;height: 100%; border: 1px black;"></div>
+  </fieldset>
+</template>
